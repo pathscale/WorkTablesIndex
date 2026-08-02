4b0664f793f26c32b6c231464b67a02766865f17
meh <no-reply@pathscale.com>
2026-08-03 02:19:41 +0700

    fix: hash pairs by key

--- src/core/pair.rs
+++ src/core/pair.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 use std::borrow::Borrow;
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[derive(Debug, Default, Clone, Hash)]
+#[derive(Debug, Default, Clone)]
 pub struct Pair<K, V>
 {
     pub key: K,
@@ -37,6 +37,15 @@ where
 {
     fn cmp(&self, other: &Self) -> Ordering {
         self.key.cmp(&other.key)
+    }
+}
+
+impl<K, V> std::hash::Hash for Pair<K, V>
+where
+    K: std::hash::Hash,
+{
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.key.hash(state);
     }
 }
 
